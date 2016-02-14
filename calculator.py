687673f270b34d4e390a39a7ee247c83a0017c7a
Elice Greenberg <egreenberg3@mail.valenciacollege.edu>
2016-02-14 17:48:23 -0500

    Week 6- Elice Greenberg

--- calculator.py
+++ calculator.py
@@ -1,20 +1,35 @@
+# Addition function (returns the sum of first and second)
+
 def add( first, second):
-    # TODO:
-    # there's an error in this code, fix it
-    return first plus second
+    return first + second
+
+
+
+# Subtraction function (returns the difference between first and second)
 
 def subtract( first, second):
-    # TODO:
-    # fill in code here that will return the difference between first and second
+    return first - second
+
+
+
+# Multiplication function (returns the product of first and second)
 
 def multiply( first, second):
-    # TODO:
-    # fill in code here that will return the product of first and second
+    return first * second
+
+
+
+# Division function (returns the quotient of first and second)
 
 def divide( first, second):
-    # TODO:
-    # fill in code here that:
-    #   1. checks the second number to see if it is zero
-    #   2. if the second number is zero, an exception is raised, the exception text must say exactly the following (make sure everything including casing and spaces match)
-    #       I'm sorry, I can't divide by zero
-    #   3. returns the quotient of first and second
+    if second == 0:
+    #checks the second number to see if it is zero
+        raise Exception('I\'m sorry, I can\'t divide by zero')
+    #defines what is in parantheses as the except statement (object) 
+    try:
+        return first / second
+    #attempts to returns the quotient of first and second (will succeed if second num isn't 0)
+    except Exception as err:
+    #stores the except statement (object) in a variable named err
+        return print(str(err))
+    #returns exception statement(object) onverted to a str and passed to a print function for proper display
